4bac30d0b78a0b6bdd7a5002b30f69f7e48f883e
Lenovo <vivekmane9860@gmail.com>
2025-06-11 22:18:06 +0530

    na

---
+++ src/Pages/DashBoard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Dashboard = () => {
+    return (
+        <div className="p-1 text-gray-900 dark:text-white">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
