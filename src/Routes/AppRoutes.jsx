0cac00d70fe04186ddef5190696325fae0c7989c
Lenovo <vivekmane9860@gmail.com>
2025-06-11 15:36:06 +0530

    protected routes changes

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,23 +1,23 @@
-import {Routes, Route, Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 import DashBoard from "../Pages/DashBoard.jsx";
 import LandingPage from "../Components/LandingPage.jsx";
 import OAuthSuccess from "../Components/OAuthSuccess.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
-const AppRoutes = ()=> {
+import Register from "../Components/Register.jsx";
+
+const AppRoutes = () => {
     return (
-
         <Routes>
+            <Route path="/" element={<LandingPage/>}/>
+            <Route path="/oauth-success" element={<OAuthSuccess/>}/>
 
-            <Route path="/" element={<LandingPage/>} />
-            <Route path="/oauth-success" element={<OAuthSuccess />} />
-            <Route path="/dashboard" element={
-                <ProtectedRoute>
-                <DashBoard/>
-                </ProtectedRoute>
-            } />
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute />}>
+            <Route path="/login" element={<Register/>}/>
+                <Route path="/dashboard" element={<DashBoard/>}/>
+            </Route>
         </Routes>
-
     );
 };
 export default AppRoutes;
