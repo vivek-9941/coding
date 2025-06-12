ec6c3fcb01c8ac8149decf1a2e84156aeadb0607
Lenovo <vivekmane9860@gmail.com>
2025-06-12 18:41:52 +0530

    adding all contexts

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,25 @@ import './index.css'
 import App from './App.jsx'
 import {ThemeProvider} from "./Context/ThemeContext.jsx";
 import {UserProvider} from "./Context/UserContext.jsx";
+import LeetcodeProvider from "./Context/LeetcodeContext.jsx";
+import {BrowserRouter} from "react-router-dom";
+import CodeforcesProvider from "./Context/CodeforcesContext.jsx";
+import CodechefProvider from "./Context/CodechefContext.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
+
         <ThemeProvider>
             <UserProvider>
-                <App/>
+                <LeetcodeProvider>
+                    <CodeforcesProvider>
+                        <CodechefProvider>
+                            <BrowserRouter>
+                                <App/>
+                            </BrowserRouter>
+                        </CodechefProvider>
+                    </CodeforcesProvider>
+                </LeetcodeProvider>
             </UserProvider>
         </ThemeProvider>
-    </StrictMode>,
+
 )
