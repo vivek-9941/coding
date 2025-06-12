d1b7ebe36b29252c7cc8840d5a77f327a458ec4f
Lenovo <vivekmane9860@gmail.com>
2025-06-12 18:41:06 +0530

    codechef context creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import AppRoutes from "./Routes/AppRoutes.jsx";
 import {Toaster} from "react-hot-toast";
 import {ToastContainer} from "react-toastify";
 
 function App() {
     return (
-        <Router>
-            <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
-                <ToastContainer />
-                <AppRoutes />
-            </div>
-        </Router>
+
+        <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+            <ToastContainer/>
+            <AppRoutes/>
+        </div>
     );
 }
 
