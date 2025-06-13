68e4c091a60fd58dc047abb69306b4ac3acd0b0a
Lenovo <vivekmane9860@gmail.com>
2025-06-13 14:31:37 +0530

    removed all the platforms context those were wrapping <App />

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import {ThemeProvider} from "./Context/ThemeContext.jsx";
 import {UserProvider} from "./Context/UserContext.jsx";
-import LeetcodeProvider from "./Context/LeetcodeContext.jsx";
 import {BrowserRouter} from "react-router-dom";
-import CodeforcesProvider from "./Context/CodeforcesContext.jsx";
-import CodechefProvider from "./Context/CodechefContext.jsx";
+
 
 createRoot(document.getElementById('root')).render(
+    <StrictMode>
+    <ThemeProvider>
+        <UserProvider>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
 
-        <ThemeProvider>
-            <UserProvider>
-                <LeetcodeProvider>
-                    <CodeforcesProvider>
-                        <CodechefProvider>
-                            <BrowserRouter>
-                                <App/>
-                            </BrowserRouter>
-                        </CodechefProvider>
-                    </CodeforcesProvider>
-                </LeetcodeProvider>
-            </UserProvider>
-        </ThemeProvider>
-
+        </UserProvider>
+    </ThemeProvider>
+    </StrictMode>
 )
